5376cbc78c5a617f0e8c6fb1529d1a6513ac7162
Imane-Rahoui <Imaanerahoui@gmail.com>
2022-12-20 11:55:32 +0100

    form and write NFC done

--- app/src/main/java/org/mbds/nfctag/write/NFCWriterActivity.java
+++ app/src/main/java/org/mbds/nfctag/write/NFCWriterActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -19,11 +22,16 @@ public class NFCWriterActivity extends AppCompatActivity {
 
     private NfcAdapter nfcAdapter;
     private PendingIntent pendingIntent;
-
     private NfcTagViewModel viewModel;
 
-    // TODO Analyser le code et comprendre ce qui est fait
-    // TODO Ajouter un formulaire permettant à un utilisateur d'entrer le texte à mettre dans le tag
+    Button ok;
+    EditText data;
+    View formulaire;
+    View cardReader;
+
+
+    // TODO Analyser le code et comprendre ce qui est fait // done
+    // TODO Ajouter un formulaire permettant à un utilisateur d'entrer le texte à mettre dans le tag // done
     // TODO Le texte peut être 1) une URL 2) un numéro de téléphone 3) un plain texte
     // TODO Utiliser le view binding
     // TODO L'app ne doit pas crasher si les tags sont mal formattés
@@ -32,6 +40,11 @@ public class NFCWriterActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.write_tag_layout);
+
+        ok = findViewById(R.id.submit);
+        data = findViewById(R.id.data);
+        formulaire = findViewById(R.id.formulaire);
+        cardReader = findViewById(R.id.card_reader);
 
         // init ViewModel
         viewModel = new ViewModelProvider(this).get(NfcTagViewModel.class);
@@ -47,6 +60,16 @@ public class NFCWriterActivity extends AppCompatActivity {
         pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).
                 addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
         // single top flag avoids activity multiple instances launching
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // Toast.makeText(NFCWriterActivity.this, data.getText(), Toast.LENGTH_SHORT).show();
+                formulaire.setVisibility(View.GONE);
+                cardReader.setVisibility(View.VISIBLE);
+
+            }
+        });
     }
 
     @Override
@@ -99,18 +122,19 @@ public class NFCWriterActivity extends AppCompatActivity {
      */
     @Override
     public void onNewIntent(Intent intent) {
-
-        super.onNewIntent(intent);
-        String action = intent.getAction();
-        // check the event was triggered by the tag discovery
-        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
-                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
-                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
-            // get the tag object from the received intent
-            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            viewModel.writeTag("hello MBDS", tag, TagType.TEXT);
-        } else {
-            // TODO Indiquer à l'utilisateur que ce type de tag n'est pas supporté
+        if (formulaire.getVisibility()==View.GONE) {
+            super.onNewIntent(intent);
+            String action = intent.getAction();
+            // check the event was triggered by the tag discovery
+            if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                    || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+                    || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+                // get the tag object from the received intent
+                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                viewModel.writeTag(data.getText().toString(), tag, TagType.TEXT);
+            } else {
+                // TODO Indiquer à l'utilisateur que ce type de tag n'est pas supporté
+            }
         }
     }
 }
